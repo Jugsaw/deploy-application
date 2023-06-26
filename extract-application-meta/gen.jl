5d9fbb3f5fca0949930699e9b4f319070ce327fc
Jun Tian <tianjun.cpp@gmail.com>
2023-06-26 18:03:20 +0800

    debug

--- extract-application-meta/gen.jl
+++ extract-application-meta/gen.jl
@@ -5,7 +5,7 @@ cd(get(ENV, "INPUT_DIR", "."))
 Pkg.activate(".")
 Pkg.instantiate()
 
-include("./app.jl")
+include(joinpath(pwd(), "app.jl")) # !!! without joinpath, `app.jl` is resolved to `/app.jl`
 
 using Jugsaw.Template: docker_config
 
