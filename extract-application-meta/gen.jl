37abaae2bbf78b24c6176a490a7930b103f081f5
Jun Tian <tianjun.cpp@gmail.com>
2023-06-26 18:53:38 +0800

    debug

--- extract-application-meta/gen.jl
+++ extract-application-meta/gen.jl
@@ -1,18 +1,21 @@
 using Pkg
 
-cd(get(ENV, "INPUT_DIR", "."))
+# cd(get(ENV, "INPUT_DIR", "."))
 
-Pkg.activate(".")
-Pkg.instantiate()
+# Pkg.activate(".")
+# Pkg.instantiate()
 
-include(joinpath(pwd(), "app.jl")) # !!! without joinpath, `app.jl` is resolved to `/app.jl`
+# include(joinpath(pwd(), "app.jl")) # !!! without joinpath, `app.jl` is resolved to `/app.jl`
 
-using Jugsaw.Template: docker_config
-using JugsawIR: julia2ir
+# using Jugsaw.Template: docker_config
+# using JugsawIR: julia2ir
 
-dockerfile = docker_config(; juliaversion=VERSION)
-demos, types = julia2ir(app)
-demos, types = escape_string(demos), escape_string(types)
+# dockerfile = docker_config(; juliaversion=VERSION)
+# demos, types = julia2ir(app)
+# demos, types = escape_string(demos), escape_string(types)
+
+dockerfile = "A"
+demos, types = "B", "C"
 
 # outputs
 DEMOS = "demos"
